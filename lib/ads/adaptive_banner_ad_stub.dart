8d02a9bd589f3b5adf2c19a03d04097f27851969
John Ace Colani <168901261+johnacolani@users.noreply.github.com>
2026-08-31 07:22:22 -0400

    Add non-mobile banner stub

---
+++ lib/ads/adaptive_banner_ad_stub.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AdaptiveBannerAd extends StatelessWidget {
+  const AdaptiveBannerAd({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SizedBox.shrink();
+}
